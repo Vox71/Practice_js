4d2bc0066b8a19b7eb246faa5b0e694a4bc32b5e
Mikhail Volkov <mikellevolkov@gmail.com>
2023-09-22 17:05:42 +0300

    Refactored v1

--- task1.js
+++ task1.js
@@ -1,24 +1,20 @@
 class User {
-
   constructor(name, age, address) {
     this.name = name;
 	this.age = age;
 	this.address = address;
   }
-
-  sayHi() {
-	console.log("Инфа о пользователе: " + this.name + " " + this.age + " " + this.address);
+  getInfo() {
+	console.log(`Имя: ${this.name} | Возраст: ${this.age} | Адрес: ${this.address}`);
   }
-  
 }
-
 
 function getTotalAge(list){
 	var totalAge = 0;
 	for (let i = 0; i < list.length; i++){
 		totalAge += list[i].age;
 	}
-return totalAge;
+	return totalAge;
 }
 
 function getUsersStreets(list){
@@ -26,7 +22,7 @@ function getUsersStreets(list){
 	for (let i = 0; i < list.length; i++){
 		usersStreets.push(list[i].address[1])
 	}
-return usersStreets;
+	return usersStreets;
 }
 
 function getOldPeople(list){
@@ -36,24 +32,29 @@ function getOldPeople(list){
 		oldPeople.push(list[i]);
 		}
 	}
-return oldPeople;
+	return oldPeople;
 }
 
 let Users = [
 	user1 = new User("Миха", 13, ['Dolgoprudny', 'Molodejnaya', '14k3']),
-	user1 = new User("Михаил", 69, ['Dolgoprudny', 'Molodejnaya', '14k3']),
-	user1 = new User("Микелле", 180, ['Brasov', 'Strada Transilvaniei', '6']),
-	user2 = new User("Лизон Пузон", 9, ['Lobnya', 'Vorovskaya street', '24'])
+	user2 = new User("Михаил", 69, ['Dolgoprudny', 'Molodejnaya', '14k3']),
+	user3 = new User("Микелле", 180, ['Brasov', 'Strada Transilvaniei', '6']),
+	user4 = new User("Лизон Пузон", 9, ['Lobnya', 'Vorovskaya street', '24'])
 ]
 
-
-var totalAge = getTotalAge(Users);
+var totalAge = getTotalAge(Users); 
 console.log(totalAge);
+/*getTotalAge проходится в цикле по всем объектам 
+  в переданном массиве и суммирует Age*/
 
-var usersStreets = getUsersStreets(Users);
+var usersStreets = getUsersStreets(Users); 
 console.log(usersStreets);
+/*getUserStreets в цикле записывает в возвращаемый 
+  массив все названия улиц (находится в User.address[1])*/
 
 var oldPeople = getOldPeople(Users);
 for(let i = 0; i < oldPeople.length; i++){
-	oldPeople[i].sayHi();
+	oldPeople[i].getInfo();
 }
+/*getOldPeople запускает цикл, в котором проверяются все объекты в 
+переданном массиве, и если .age > 60, то передает этот объект в вохвращаемый массив*/
--- task2.js
+++ task2.js
@@ -1,21 +1,21 @@
 class User_ids {
-    constructor(ID, name, age) {
-      this.ID = ID;
-      this.name = name;
-      this.age = age;
-    }
-    sayHi() {
-      console.log(`ID: ${this.ID} | Имя: + ${this.name} | Возраст: ${this.age}`);
-    }
+  constructor(ID, name, age) {
+    this.ID = ID;
+    this.name = name;
+    this.age = age;
   }
+  getInfo() {
+    console.log(`ID: ${this.ID} | Имя: ${this.name} | Возраст: ${this.age}`);
+  }
+}
 
 let userList = [
-    user1 = new User_ids(1, "Vladislav", 12),
-    user2 = new User_ids(2, "Oleg", 33),
-    user3 = new User_ids(13, "Danila", 44),
-    user4 = new User_ids(1337, "Kiril", 23),
-    user5 = new User_ids(3, "Reimu", 18),
-    user6 = new User_ids(4, "Marisa", 14)
+  user1 = new User_ids(1, "Vladislav", 12),
+  user2 = new User_ids(2, "Oleg", 33),
+  user3 = new User_ids(13, "Danila", 44),
+  user4 = new User_ids(1337, "Kiril", 23),
+  user5 = new User_ids(3, "Reimu", 18),
+  user6 = new User_ids(4, "Marisa", 14)
 ]
 
 function sleep(seconds) {
@@ -26,7 +26,6 @@ getUser = async (ID_num) => {
    await sleep(1.3);
    console.log('Загружен пользователь с ID ' + ID_num);
    return userList.find((element) => element.ID == ID_num);
-   
 }
 
 loadUsersSquentially = async (ID_list) => {
@@ -36,10 +35,9 @@ loadUsersSquentially = async (ID_list) => {
     loadedUsers.push(await getUser(ID_list[i]));
   }
   console.log(`Пользователи загружены за ${(Date.now() - t0) /1000} секунд`);
-  await sleep(1);
-  console.log('Список пользователей:')
+  console.log('Список пользователей:');
   for(let i = 0; i < loadedUsers.length; i++){
-    loadedUsers[i].sayHi();
+    loadedUsers[i].getInfo();
   }
   return loadedUsers;
 }
@@ -52,28 +50,24 @@ loadUsersInParallel = async (ID_list) => {
     loadedUsers.push(await p);
   }
   console.log(`Пользователи загружены за ${(Date.now() - t0) /1000} секунд`);
-  await sleep(1);
-  console.log('Список пользователей:')
+  console.log('Список пользователей:');
   for(let i = 0; i < loadedUsers.length; i++){
-    loadedUsers[i].sayHi();
+    loadedUsers[i].getInfo();
   }
   return loadedUsers;
-
 }
 
-var main = async () =>{
+var main = async () =>{ //создаем асинхронный main(), что бы можно было прописать await перед функциями с promise
   console.log(await getUser(1));  //Поиск одного пользователя с ID 1. 
   //На выходе из промиса получаем объект класса User_ids
   
   let primeUsers = [1, 1337, 3];
 
-  console.log(await loadUsersSquentially(primeUsers));//Поиск пользователей с ID, заданными в массиве primeUsers
-  //Почередно запускаем getUser для каждого ID и записываем резолв из промиса в возвращаемый функцией массив 
+  await loadUsersSquentially(primeUsers);//Поиск пользователей с ID, заданными в массиве primeUsers
+  //Почередно запускаем getUser для каждого ID и записываем резолв из промиса в возвращаемый функцией массив объектов класса User_ids
   
-  console.log(await loadUsersInParallel(primeUsers));//Поиск пользователей с ID, заданными в массиве primeUsers
-  //Параллельно запускаем getUser для каждого ID и записываем резолв из промиса в возвращаемый функцией массив 
+  await loadUsersInParallel(primeUsers);//Поиск пользователей с ID, заданными в массиве primeUsers
+  //Параллельно запускаем getUser для каждого ID и записываем резолв из промиса в возвращаемый функцией массив объектов класса User_ids
 }
-
-
 
 main();
